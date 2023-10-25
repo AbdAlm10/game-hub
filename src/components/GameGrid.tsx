f1e78acdc77e74b792d4559d5a7cf016ecd39ed2
So Na <you@example.com>
2023-10-25 17:48:47 +0300

    Clean up the games cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text color={"red"}>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} p={10} spacing={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} p={10} spacing={6}>
         {isLoding &&
           skeletons.map((skeletons) => (
             <GameCardContainer key={skeletons}>
